a2a73649ec1241f7b553b3bbb15887f8c6225da0
ridho-code <60309031+ridho-code@users.noreply.github.com>
2024-05-27 18:55:11 +0800

    add transaction

--- admin/pages/transaksi.php
+++ admin/pages/transaksi.php
@@ -25,10 +25,8 @@ $tableName = "view_transaksi";
             <thead>
                 <tr>
                     <th>No. </th>
-                    <th>Nama</th>
-                    <th>Jumlah</th>
-                    <th>Harga Satuan</th>
-                    <th>Total Harga</th>
+                    <th>Nama Produk</th>
+                    <th>Nama User</th>
                 </tr>
             </thead>
             <tbody>
@@ -41,11 +39,8 @@ $tableName = "view_transaksi";
                 <tr>
                     <td><?= $count++?>.</td>
                     <td><?= $data['nama_produk']?></td>
-                    <td><?= $data['jumlah']?></td>
-                    <td>Rp. <?= number_format($data['harga_produk'])?></td>
-
-                    <td>Rp. <?= number_format($data['total_harga'])?></td>
-                   
+                    <td><?= $data['nama_user']?></td>
+                    
                 </tr>
                 <?php
                 }
@@ -54,104 +49,7 @@ $tableName = "view_transaksi";
         </table>
     </div>
 </div>
-<!-- Tambah -->
-<div class="modal fade" id="tambah-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="modal-heading">Tambah Data</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
 
-            <form method="post" enctype="multipart/form-data">
-                <div class="modal-body ">
-                    <div class="row">
-                        <div class="col">
-                            <label for="">Nama</label>
-                            <input type="text" class="form-control" id="tnama" value="">
-                        </div>
-                        <div class="col">
-                            <label for="" class="mt-">Harga</label>
-                            <input type="number" class="form-control" id="tharga" value="">
-                        </div>
-                    </div>
-
-                    <div class="py-2">
-                        <label for="" class="mt-">Jumlah</label>
-                        <input type="number" class="form-control" id="tjumlah" value="">
-                    </div>
-
-
-                    <p for="" class="mt-3">Foto</p>
-                    <!-- <img src="uploads/img/" alt="" style="width:150px; height: 100px"> -->
-                    <input type="file" class="form-control mt-3" id="tfoto" onchange="validasiFile()">
-
-                    <div class="col-md-2 mb-2 mt-3" id="pratinjauGambar"></div>
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                    <button type="button" class="btn btn-primary" onclick="tambah()">Tambah</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-<!-- Update -->
-<div class="modal fade" id="edit-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="modal-heading">Edit Data</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-
-            <form method="post" enctype="multipart/form-data">
-                <div class="modal-body ">
-                    <div class="row">
-                        <input type="text" id="uid" class="form-control" hidden disabled>
-                        <div class="row">
-                            <div class="col">
-                                <label for="">Nama</label>
-                                <input type="text" class="form-control" id="unama" value="">
-                            </div>
-                            <div class="col">
-                                <label for="" class="mt-">Harga</label>
-                                <input type="number" class="form-control" id="uharga" value="">
-                            </div>
-                        </div>
-
-                        <div class="py-2">
-                            <label for="" class="mt-">Jumlah</label>
-                            <input type="number" class="form-control" id="ujumlah" value="">
-                        </div>
-
-                        <div class="py-2">
-                        <label for="" class="">Foto</label>
-                    <input type="file" class="form-control mt-3" id="ufoto" onchange="return validasiFile1()">
-
-                        </div>
-                    
-
-                        <div class="row">
-                            <div class="col-md mt-3" id="pratinjauGambar0">
-
-                            </div>
-
-                            <div class="col-md mt-3" id="pratinjauGambar1"></div>
-                        </div>
-
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                        <button type="button" class="btn btn-primary" onclick="edit()">Edit</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
 
 
 <script src="pages/transaksi.js"></script>
